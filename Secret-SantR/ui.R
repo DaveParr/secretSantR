0ccbbcd1464d0360e5e4c6fbe9357af57b753c96
DaveRGP <DavidRGParr@gmail.com>
2016-11-26 21:16:05 +0000

    remove unused inputs

--- Secret-SantR/ui.R
+++ Secret-SantR/ui.R
@@ -13,7 +13,5 @@ library(shiny)
 shinyUI(fluidPage(
   DT::dataTableOutput("responses", width = 300), tags$hr(),
   textInput("name", "Name", ""),
-  checkboxInput("used_shiny", "I've built a Shiny app in R before", FALSE),
-  sliderInput("r_num_years", "Number of years using R", 0, 25, 2, ticks = FALSE),
   actionButton("submit", "Submit")
 ))
