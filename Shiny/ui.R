4d48f376b271cf3659c8c15763755b65c9d257b5
DaveRGP <DavidRGParr@gmail.com>
2016-12-10 16:46:30 +0000

    use well for form filling

--- Shiny/ui.R
+++ Shiny/ui.R
@@ -1,13 +1,18 @@
 library(shiny)
 library(shinythemes)
 
-shinyUI(fluidPage(theme = shinytheme("paper"),
-  shinyjs::useShinyjs(),
-  h1("Get your secret santa topic!"),
-  textInput("name", "Name", ""),
-  h2(tagList("Talk about:",textOutput("topic"))),
-  actionButton("mulligan", "Give me another"),
-  actionButton("submit", "This one's fine", class = "btn-primary"),
-  h3("Assigned Topics"),
-  DT::dataTableOutput("responses", width = "50%")
-))
+shinyUI(
+  fluidPage(
+    theme = shinytheme("paper"),
+    shinyjs::useShinyjs(),
+    h1("Get your secret santa topic!"),
+    h2(tagList("Talk about:", textOutput("topic"))),
+    wellPanel(
+      textInput("name", "Name", ""),
+      actionButton("mulligan", "Give me another"),
+      actionButton("submit", "This one's fine", class = "btn-primary")
+      , width = 300),
+    h3("Assigned Topics"),
+    DT::dataTableOutput("responses", width = 300)
+  )
+)
