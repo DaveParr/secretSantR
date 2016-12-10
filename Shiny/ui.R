283c5bee7c13042c9908675a96bb514f8c6a639a
DaveRGP <DavidRGParr@gmail.com>
2016-12-10 17:10:55 +0000

    use wiell panels to indicate forms filling and use fluid row layout

--- Shiny/ui.R
+++ Shiny/ui.R
@@ -1,18 +1,24 @@
 library(shiny)
 library(shinythemes)
 
-shinyUI(
-  fluidPage(
-    theme = shinytheme("paper"),
-    shinyjs::useShinyjs(),
-    h1("Get your secret santa topic!"),
-    h2(tagList("Talk about:", textOutput("topic"))),
+shinyUI(fluidPage(
+  shinythemes::themeSelector(),
+  shinyjs::useShinyjs(),
+  titlePanel("Get your secret santa topic!"),
+  tags$hr(),
+  fluidRow(column(
+    6,
+    h3(tagList("Talk about:", textOutput("topic"))),
     wellPanel(
       textInput("name", "Name", ""),
       actionButton("mulligan", "Give me another"),
       actionButton("submit", "This one's fine", class = "btn-primary")
-      , width = 300),
-    h3("Assigned Topics"),
-    DT::dataTableOutput("responses", width = 300)
-  )
-)
+      ,
+      width = 300
+    )
+  ),
+  fluidRow(column(
+    6, h3("Assigned Topics"),
+    DT::dataTableOutput("responses")
+  )))
+))
